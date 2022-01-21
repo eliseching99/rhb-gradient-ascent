9150359d2c00fcc8e1f23aaa326b95ef947a7b1d
Elise Ching <eliseching1999@gmail.com>
2022-01-22 01:50:39 +0800

    add pie chart

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import streamlit as st
 import pandas as pd
+# library
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(6, 6))
+wedgeprops = {'width':0.3, 'edgecolor':'black', 'linewidth':3}
+#show number of average vs target
+ax.pie([23,73], wedgeprops=wedgeprops, startangle=90, colors=['#5DADE2', '#515A5A'])
+plt.text(0, 0, "23%", ha='center', va='center', fontsize=42)
+
+fig2, ax2 = plt.subplots(figsize=(6, 6))
+wedgeprops = {'width':0.3, 'edgecolor':'black', 'linewidth':3}
+#show number of average vs target
+ax2.pie([30,70], wedgeprops=wedgeprops, startangle=90, colors=['#FFC0CB', '#515A5A'])
+plt.text(0, 0, "30%", ha='center', va='center', fontsize=42)
+
+
+fig3, ax3 = plt.subplots(figsize=(6, 6))
+wedgeprops = {'width':0.3, 'edgecolor':'black', 'linewidth':3}
+#show number of average vs target
+ax3.pie([50,50], wedgeprops=wedgeprops, startangle=90, colors=['#77dd77', '#515A5A'])
+plt.text(0, 0, "50%", ha='center', va='center', fontsize=42)
+
+
 st.title('Budget Buddy 💰')
 st.metric("Current Account", "Rm15,000", "-RM2,500")
+month = st.number_input('Choose month',1,12)
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Food")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+    st.pyplot(fig)
+    with st.expander("Change"):
+        foodBudget = st.slider(
+        'Select new target budget for Food',
+        0, 100,25)
 
 with col2:
-    st.header("Entertainment")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
+    st.subheader("Entertainment")
+    st.pyplot(fig2)
+    with st.expander("Change"):
+        entertainmentBudget = st.slider(
+        'Select new target budget for Entertainment',
+        0, 100,25)
+
+col3,col4 = st.columns(2)
 
 with col3:
-    st.header("E-wallet")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
-# col2.metric("Wind", "9 mph", "-8%")
-# col3.metric("Humidity", "86%", "4%")
+    st.subheader("E-wallet")
+    st.pyplot(fig3)
+    with st.expander("Change"):
+        walletBudget= st.slider(
+        'Select new target budget for E Wallet',
+        0, 100,25)
+
 df= pd.read_csv("sample_creditcard_txn.csv")
-print(df.head)
 df['TRANSACTION_DATE'] = pd.to_datetime(df['TRANSACTION_DATE'], errors='coerce')
 
-df2 = df[df['TRANSACTION_DATE'].dt.month == 11]
-print(df2.head)
+## Getting a particular customer info
+customerRecords=df[df['cust_number'] == 6]
+print(customerRecords)
+customerRecords = customerRecords[customerRecords['TRANSACTION_DATE'].dt.month == 4]
+st.dataframe(customerRecords)
